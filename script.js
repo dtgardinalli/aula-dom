0f4e82417c99ce901898cd89380d9a04544ccf6a
Daniela Gardinalli <daniela.gardinalli>
2019-06-13 16:55:16 -0300

    colocar audio

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const maisa = document.querySelector("img");
-const lista = document.querySelector ("ul");
+const lista = document.querySelector("ul");
+const roleta = document.querySelector("main img");
 
 function abrirMenu(){
     lista.classList.toggle ("ativo");
 }
 
 maisa.onclick = abrirMenu;
+
+function rodaARoda(){
+    roleta.classList.toggle ("roda-jequiti");
+    h1.innerHTML = "É PERUUUUUUUUUCAAA!"
+}
+
+roleta.onclick = rodaARoda;
+
+{
+    
+}
+
